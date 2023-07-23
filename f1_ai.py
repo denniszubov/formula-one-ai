b0f881927afec439b28c8aaef1b49546539f3fab
dzubov3 <dennis.zubov@gatech.edu>
2023-07-22 21:24:30 -0700

    Add basic formula one ai object

---
+++ f1_ai.py
@@ -0,0 +1,19 @@
+import openai
+
+
+class FormulaOneAI:
+    def __init__(self, api_key, gpt_model="gpt-4"):
+        self.api_key = api_key
+        openai.api_key = self.api_key
+
+        self.gpt_model = gpt_model
+        self.messages = [{"role": "system", "content": "You are a helpful assistant."}]
+
+    def ask(self, prompt, max_tokens=100):
+        response = openai.ChatCompletion.create(
+            model=self.gpt_model,
+            messages=self.messages,
+            function=[],
+            max_tokens=max_tokens,
+        )
+        return response["choices"][0]["message"]["content"]
