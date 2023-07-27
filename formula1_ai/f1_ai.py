78f72b683673f234fa533c72548a6f4c95d298c5
dzubov3 <dennis.zubov@gatech.edu>
2023-07-26 20:21:19 -0700

    Add function calling ability

--- formula1_ai/f1_ai.py
+++ formula1_ai/f1_ai.py
@@ -1,4 +1,8 @@
+import json
+from typing import Any, Tuple
+
 import openai
+from functions import function_schemas, get_driver_standings
 
 
 class FormulaOneAI:
@@ -8,12 +12,44 @@ class FormulaOneAI:
 
         self.gpt_model = gpt_model
         self.messages = [{"role": "system", "content": "You are a helpful assistant."}]
+        self.function_schema = function_schemas
+        self.function_mapping = {"get_driver_standings": get_driver_standings}
 
-    def ask(self, prompt, max_tokens=100):
+    def ask(self, prompt):
         self.messages.append({"role": "user", "content": prompt})
+        response = self._chat_completion()
+        if response.get("function_call"):
+            function_name, kwargs = self.parse_response(response)
+            func = self.function_mapping[function_name]
+            function_response = func(**kwargs)
+
+            self.messages.append(response)  # extend conversation with assistant's reply
+            self.messages.append(
+                {
+                    "role": "function",
+                    "name": function_name,
+                    "content": str(function_response),
+                }
+            )
+
+            second_response = self._chat_completion()
+            return second_response["content"]
+        else:
+            return response["content"]
+
+    def _chat_completion(self) -> dict[str, Any]:
         response = openai.ChatCompletion.create(
             model=self.gpt_model,
             messages=self.messages,
-            max_tokens=max_tokens,
+            functions=self.function_schema,
+            max_tokens=100,
         )
-        return response["choices"][0]["message"]["content"]
+        message = response["choices"][0]["message"]
+
+        self.messages.append(message)
+        return message
+
+    def parse_response(self, message: dict[str, Any]) -> Tuple[str, Any]:
+        return message["function_call"]["name"], json.loads(
+            message["function_call"]["arguments"]
+        )
--- formula1_ai/frontend.py
+++ formula1_ai/frontend.py
@@ -1,6 +1,5 @@
 import os
 
-import pandas as pd
 import streamlit as st
 from dotenv import load_dotenv
 from f1_ai import FormulaOneAI
@@ -12,14 +11,11 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 f1_ai = FormulaOneAI(openai_api_key)
 
 st.write("Ask anything about F1")
-question = st.text_input(
-    "Formula 1 Question:", "When was the last time Fernando Alonso won a race?"
-)
+question = st.text_input("Formula 1 Question:", "What is the current driver standings?")
 
 if st.button("Submit"):
     response = f1_ai.ask(question)
     st.markdown(response)
 
-st.write(
-    pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
-)
+st.write("Messages")
+st.write(f1_ai.messages)
