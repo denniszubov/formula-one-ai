ae78a1b5eaf0974314e72b144c6805e1630d4871
dzubov3 <dennis.zubov@gatech.edu>
2023-07-26 19:57:10 -0700

    Add first F1 function

---
+++ formula1_ai/functions.py
@@ -0,0 +1,25 @@
+from typing import List, Tuple
+
+import requests
+
+
+def get_driver_standings() -> List[Tuple[int, str]]:
+    url = "http://ergast.com/api/f1/current/driverStandings.json"
+
+    response = requests.get(url)
+    data = response.json()
+    drivers = data["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"]
+
+    drivers_family_name = [
+        (i + 1, x["Driver"]["familyName"]) for i, x in enumerate(drivers)
+    ]
+    return drivers_family_name
+
+
+function_schemas = [
+    {
+        "name": "get_driver_standings",
+        "description": "Get the driver standings",
+        "parameters": {"type": "object", "properties": {}, "required": []},
+    }
+]
