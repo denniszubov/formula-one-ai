78292d5288247242f8c2020691f1e6fc87fcfde5
dzubov3 <dennis.zubov@gatech.edu>
2023-07-28 20:24:23 -0700

    Update system prompt to give context on functions

--- f1/prompts.py
+++ f1/prompts.py
@@ -3,7 +3,11 @@ from datetime import date
 SYSTEM_CONTENT = f"""You are a helpful assistant that will answer
 Formula 1 related queries.
 
-You have access to functions that fetch F1 data.
+You have access to functions that fetch F1 data. Nearly all functions will
+have a `season` parameter and a `round` parameter. The season parameter is
+a 4 digit int representing the year of the season for the query. The round
+parameter is a 1 or 2 digit int representing the race within the season for
+the query.
 
 Today is {date.today()}
 """
