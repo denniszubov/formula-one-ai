d8e9d8bd3cb797e1515e9332a1c6a6c66d859553
Dennis Zubov <60840320+denniszubov@users.noreply.github.com>
2023-07-30 18:36:02 -0700

    Add standings api calls (#2)

--- f1/functions.py
+++ f1/functions.py
@@ -1,9 +1,12 @@
+from typing import Any, Callable
+
 import pandas as pd
 import requests
 
 BASE_URL = "http://ergast.com/api/f1"
 
 
+# Standings functions
 def get_driver_standings(season: int, round: int = 0) -> pd.DataFrame:
     """Get the driver standings at the end of a specific season or
     after a specific round in a season. If the round parameter is not
@@ -78,7 +81,8 @@ def get_constructors_standings(season: int, round: int = 0) -> pd.DataFrame:
     return constructors_standings
 
 
-def get_season_info(season: int) -> pd.DataFrame:
+# Season List functions
+def get_season_info(season: int, cols: list[str]) -> pd.DataFrame:
     """Get information about a specific F1 season. This will return
     all the races in the season, with information about round number,
     race name, date, circuit name, and country.
@@ -86,6 +90,10 @@ def get_season_info(season: int) -> pd.DataFrame:
     Args:
         season (int): used to specify the year in which
             to fetch the season info
+        cols (list[str]): list of strings representing which columns
+            to return. The columns that are available to return are
+            ("round_number", "race_name", "date", "circuit_name", "country").
+            Only ask for the columns that you need, the less, the better.
 
     Return:
         pd.DataFrame: a dataframe representing the season info with
@@ -104,17 +112,81 @@ def get_season_info(season: int) -> pd.DataFrame:
     dates = [x["date"] for x in races]
     circuit_names = [x["Circuit"]["circuitName"] for x in races]
     countries = [x["Circuit"]["Location"]["country"] for x in races]
-    season_info = pd.DataFrame(
-        {
-            "round_number": round_numbers,
-            "race_name": race_names,
-            "date": dates,
-            "circuit_name": circuit_names,
-            "country": countries,
-        }
-    )
 
+    list_mapping = {
+        "round_number": round_numbers,
+        "race_name": race_names,
+        "date": dates,
+        "circuit_name": circuit_names,
+        "country": countries,
+    }
+    season_info_dict = {}
+    for col in cols:
+        season_info_dict[col] = list_mapping[col]
+    season_info = pd.DataFrame(season_info_dict)
     return season_info
 
 
-f1_data = [get_driver_standings, get_constructors_standings, get_season_info]
+# Driver Information functions
+def get_driver_information(season: int = 0, round: int = 0) -> pd.DataFrame:
+    """Get driver information for the whole history of F1, for a season,
+    or for a specific round in a season.
+
+    The driver information contains a driver_id which is used to identify
+    drivers in other functions. You can use this function to get a list of all
+    drivers info to find the driver_id of a specific driver.
+
+    If you want to get all driver info, do not specify season or round. To
+    get info for a season, specify season only. If you want driver info for a
+    specific round, specify season and round.
+
+    Args:
+        season (int): used to specify the year in which
+            to fetch the driver info. Not required
+        round (int): used to specify the round in which
+            to fetch the driver info. Not required
+
+    Return:
+        pd.DataFrame: a dataframe representing the driver info. It contains
+        driver_id, first name, last name, date of birth, and nationality.
+    """
+    url = BASE_URL
+    if season:
+        url += f"/{season}"
+        if round:
+            url += f"/{round}"
+
+    # We are adding the `limit` arg to ensure we get all the drivers in one call.
+    # Otherwise, the limit default is 30
+
+    url += "/drivers.json?limit=10000"
+
+    response = requests.get(url)
+    data = response.json()
+
+    drivers = data["MRData"]["DriverTable"]["Drivers"]
+
+    driver_ids = [x["driverId"] for x in drivers]
+    first_names = [x["givenName"] for x in drivers]
+    last_names = [x["familyName"] for x in drivers]
+    date_of_births = [x["dateOfBirth"] for x in drivers]
+    nationalities = [x["nationality"] for x in drivers]
+    driver_info = pd.DataFrame(
+        {
+            "driver_id": driver_ids,
+            "first_name": first_names,
+            "last_name": last_names,
+            "date_of_birth": date_of_births,
+            "nationality": nationalities,
+        }
+    )
+
+    return driver_info
+
+
+f1_data: list[Callable[..., Any]] = [
+    get_driver_standings,
+    get_constructors_standings,
+    get_season_info,
+    get_driver_information,
+]
