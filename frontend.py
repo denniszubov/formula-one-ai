785547613f50388e7bbc269bfce93cfc6149867c
denniszubov <denniszubov365@gmail.com>
2023-07-31 18:15:39 -0700

    Display generated graphs

--- frontend.py
+++ frontend.py
@@ -2,6 +2,7 @@ import os
 
 import streamlit as st
 from dotenv import load_dotenv
+from PIL import Image
 
 from f1.ai import FormulaOneAI
 from f1.functions import f1_data
@@ -15,6 +16,9 @@ f1_ai = FormulaOneAI(openai_api_key, f1_data)
 st.title("Formula One AI")
 
 response = None
+
+# Directory where the graphs are stored
+dir_name = "f1/graphs"
 
 with st.form(key="my_form"):
     user_input = st.text_input(
@@ -30,8 +34,20 @@ with st.form(key="my_form"):
 if response:
     st.markdown(response)
 
-st.write("---")
+# Get list of all files inside graphs directory
+files = os.listdir(dir_name)
+
+# Get list of all png files
+graphs = [f for f in files if f.endswith(".png")]
+
+if graphs:
+    st.markdown("**Generated Graphs:**")
+    for graph in graphs:
+        file_path = os.path.join(dir_name, graph)
+        image = Image.open(file_path)
+        st.image(image)
 
 if f1_ai.messages:
+    st.write("---")
     st.markdown("**Messages:**")
     st.write(f1_ai.messages)
